466247d68269c2aa7ec060a1de8d7ab61cf800d1
Meruert <meru.forwork@gmail.com>
2024-04-18 21:12:23 +0500

    feat: solve removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,10 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copy = { ...obj };
+  keys.forEach((el) => delete copy[el]);
+  return copy;
 }
 
 /**
