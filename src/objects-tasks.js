bba955d220a185860776971763103c25d0a1494d
Meruert <meru.forwork@gmail.com>
2024-05-11 23:43:56 +0500

    feat: solve group

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -295,10 +295,30 @@ function sortCitiesArray(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const countries = Array.from(new Set(array.map(keySelector)));
+  const res = [];
+
+  countries.forEach((el) => {
+    const elObj = array.filter((item) => Object.values(item)[0] === el);
+    const cities = elObj.map(valueSelector);
+    res.push([el, cities]);
+  });
+  return res;
 }
 
+// group(
+//   [
+//     { country: 'Belarus', city: 'Brest' },
+//     { country: 'Russia', city: 'Omsk' },
+//     { country: 'Russia', city: 'Samara' },
+//     { country: 'Belarus', city: 'Grodno' },
+//     { country: 'Belarus', city: 'Minsk' },
+//     { country: 'Poland', city: 'Lodz' },
+//   ],
+//   (item) => item.country,
+//   (item) => item.city
+// );
 /**
  * Css selectors builder
  *
